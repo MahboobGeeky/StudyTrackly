7e3a03a30d949f3fc5380f7b7bca39bea6048c8a
MahboobGeeky <mahboob9184@gmail.com>
2026-09-24 11:56:13 +0530

    fixed course schema error

--- backend/src/models/Course.js
+++ backend/src/models/Course.js
@@ -2,15 +2,24 @@ import mongoose, { Schema } from "mongoose";
 
 const courseSchema = new Schema(
   {
-    userId: { type: Schema.Types.ObjectId, required: true, index: true, ref: "User" },
-    termId: { type: Schema.Types.ObjectId, required: true, index: true, ref: "Term" },
+    userId: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      index: true,
+      ref: "User",
+    },
+    termId: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      index: true,
+      ref: "Term",
+    },
     name: { type: String, required: true, trim: true },
     color: { type: String, required: true, default: "blue" },
   },
-  { timestamps: true }
+  { timestamps: true },
 );
 
 courseSchema.index({ userId: 1, termId: 1, name: 1 }, { unique: true });
 
-export const Course =
-  mongoose.models.Course || mongoose.model("Course", courseSchema);
+export const Course = mongoose.models.Course || mongoose.model("Course", courseSchema);
